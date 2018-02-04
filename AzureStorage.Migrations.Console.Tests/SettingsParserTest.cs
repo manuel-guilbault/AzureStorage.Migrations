71af13dbae4fd2e2317d9303e282508de38ec03e
Manuel Guilbault <manuel.guilbault@gmail.com>
2018-02-04 15:49:15 +0100

    Add some unit tests.

---
+++ AzureStorage.Migrations.Console.Tests/SettingsParserTest.cs
@@ -0,0 +1,37 @@
+﻿using NFluent;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace AzureStorage.Migrations.Console.Tests
+{
+    public class SettingsParserTest
+    {
+        [Fact]
+        public void Should_Parse()
+        {
+            var assembly = "c:/some/assembly/path.dll";
+            var connectionString = "some_connection_string";
+            var container = "my-container";
+            var blob = "my-blob";
+            var tags = new[] { "tag1", "tag2" };
+            var properties = new Dictionary<string, string>()
+            {
+                ["property1"] = "value1",
+                ["property2"] = "value2",
+            };
+
+            var command = $"-a {assembly} -cs {connectionString} -c {container} -b {blob} -t {string.Join(" ", tags)} -p {string.Join(" ", properties.Select(x => $"{x.Key}={x.Value}"))}";
+
+            var sut = new SettingsParser();
+            var result = sut.Parse(command.Split(' '));
+
+            Check.That(result.Assembly).Equals(assembly);
+            Check.That(result.ConnectionString).Equals(connectionString);
+            Check.That(result.Container).Equals(container);
+            Check.That(result.Blob).Equals(blob);
+            Check.That(result.Tags).ContainsExactly(tags);
+            Check.That(result.Properties).ContainsExactly(properties);
+        }
+    }
+}
